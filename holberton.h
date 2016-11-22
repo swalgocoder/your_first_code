7be4b4767f40446a1020a8e9d7e5d12952a90aee
swalgocoder <gary.d.gao@gmail.com>
2016-11-21 21:08:20 -0800

    Create holberton.h

---
+++ holberton.h
@@ -0,0 +1,25 @@
+#ifndef H_98_H
+#define H_98_H
+
+#define BUFSIZE 10
+#include <stdarg.h>
+
+typedef struct data_types
+{
+	char *valid;
+	char *(*f)();
+} d_types;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+char *(*get_valid_type(char s))(va_list);
+
+/* printf_helpers */
+char *my_char(va_list c);
+char *my_string(va_list *s);
+/*others  */
+int my_atoi(char *s);
+char *_memcpy(char *dest, char *src, unsigned int n, unsigned int bufferlen);
+int _strlen(char *s);
+void _puts(char *buffer, int size);
+#endif
